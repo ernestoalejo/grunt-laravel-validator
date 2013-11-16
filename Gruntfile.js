cac19bc13bb08e6ce4486efd98d2933492df1848
Ernesto Alejo Oltra <ernestokarim@gmail.com>
2013-11-16 20:33:04 +0100

    Import grunt bump to use it in development.

--- Gruntfile.js
+++ Gruntfile.js
@@ -80,8 +80,9 @@ module.exports = function(grunt) {
   grunt.loadTasks('tasks');
 
   // These plugins provide necessary tasks.
-  grunt.loadNpmTasks('grunt-contrib-jshint');
+  grunt.loadNpmTasks('grunt-bump');
   grunt.loadNpmTasks('grunt-contrib-clean');
+  grunt.loadNpmTasks('grunt-contrib-jshint');
   grunt.loadNpmTasks('grunt-contrib-nodeunit');
 
   // Whenever the "test" task is run, first clean the "tmp" dir, then run this
