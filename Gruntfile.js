394d979e96422664bb74f3902f65ab20d0879e57
Ernesto Alejo Oltra <ernestokarim@gmail.com>
2014-02-24 21:06:29 +0100

    Update outdated libraries.

--- Gruntfile.js
+++ Gruntfile.js
@@ -89,6 +89,8 @@ module.exports = function(grunt) {
   // plugin's task(s), then test the result.
   grunt.registerTask('test', ['clean', 'laravel_validator', 'nodeunit']);
 
+  grunt.registerTask('lint', ['jshint']);
+
   // By default, lint and run all tests.
   grunt.registerTask('default', ['jshint', 'test']);
 
